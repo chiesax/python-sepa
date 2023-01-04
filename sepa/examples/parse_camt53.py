f31aa6451944a0b357ac2538a1cf76a313415c20
Samuele Chiesa <samuele.chiesa@alpeslasers.ch>
2023-01-04 17:45:20 +0100

    compare sum

--- sepa/examples/parse_camt53.py
+++ sepa/examples/parse_camt53.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     def strip_namespace(xml):
         return re.sub(' xmlns="[^"]+"', '', xml, count=1)
 
-    with open('/home/samuele/PycharmProjects/python-sepa/sepa/examples/CAMT053_040123.xml', 'r') as f:
+    with open('/home/samuele/PycharmProjects/python-sepa/sepa/examples/CAMT053_040123_full.xml', 'r') as f:
         data_xml = f.read()
 
     camt_dict = parser.parse_string(parser.bank_to_customer_statement, bytes(strip_namespace(data_xml), 'utf8'))
@@ -39,10 +39,15 @@ if __name__ == '__main__':
         if 'entries' in statement:
             for entry in statement['entries']:
                 for ed in entry['entry_details']:
-                    for transaction in ed['transactions']:
-                        all_entries.append((transaction['refs']['account_servicer_reference'],
-                              transaction['amount']['_value'], transaction['amount']['currency'], transaction['credit_debit_indicator'],
-                              entry['additional_information'], transaction['related_parties'], entry['value_date']['date'], entry['booking_date']['date']))
+                    if 'batch' in ed and 'number_of_transactions' in ed['batch'] and int(ed['batch']['number_of_transactions']) > 1:
+                        for transaction in ed['transactions']:
+                            all_entries.append((transaction['refs']['account_servicer_reference'],
+                                  transaction['amount']['_value'], transaction['amount']['currency'], transaction['credit_debit_indicator'],
+                                  entry['additional_information'], transaction['related_parties'], entry['value_date']['date'], entry['booking_date']['date']))
+                    else:
+                        all_entries.append((None,
+                                  entry['amount']['_value'], entry['amount']['currency'], entry['credit_debit_indicator'],
+                                  entry['additional_information'], None, entry['value_date']['date'], entry['booking_date']['date']))
 
     s = sum([float(x[1]) for x in all_entries])
     pass
