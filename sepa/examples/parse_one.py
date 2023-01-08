2526427e9f22e8f80868633457bb6fec766d4aaf
Samuele Chiesa <samuele.chiesa@alpeslasers.ch>
2023-01-08 22:46:40 +0100

    parse viseca one transactions html

---
+++ sepa/examples/parse_one.py
@@ -0,0 +1,22 @@
+import itertools
+
+from bs4 import BeautifulSoup
+import re
+
+if __name__ == '__main__':
+    with open('/home/samuele/PycharmProjects/python-sepa/sepa/examples/One.html', 'r') as f:
+        d = f.read()
+    soup = BeautifulSoup(d, 'html.parser')
+
+    #print(soup.get_text())
+    st = soup.find_all('div', class_='table-item')
+    details = soup.find_all('strong', id=re.compile('^transaction-details'))
+    dates = soup.find_all(id=re.compile('transaction-date-item-date'))
+    am = soup.find_all(id=re.compile('^transaction-info-amount'))
+    curr = soup.find_all(id=re.compile('^transaction-info-currency'))
+
+    for p in itertools.zip_longest(details, dates, am, curr):
+        print([x.text for x in p])
+
+
+
